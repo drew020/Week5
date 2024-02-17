0751c7e5ad237c54ace997e40a2eb347497cb363
Andrew McClain <86428905+drew020@users.noreply.github.com>
2024-02-16 22:11:26 -0500

    display total number of possible Assignments per student

--- script.js
+++ script.js
@@ -79,30 +79,19 @@ const LearnerSubmissions = [
 ];
 //#endregion Objects and array
 
-//#region global variables
-
-//main process 
-const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-//#endregion global variables
-
 //#region main process
-console.log(result);
+const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 //#endregion main process
 
 //#region Functions
 function getLearnerData(course, ag, submissions) {
-    // Cast Argument Array to use methods
-    submission = [];
-
-    //Cast Argument to Obj to use methods
-    ag = {};
-    course = {};
 
     //Function Variables
     let vTotalNumberOfStudent = 0;
     let vUniqueLearnerIDCount = [];
+    let vTotalNumberOfAssignments = 0;
 
-    //#region display total number of students
+//#region display total number of students
     for (let i = 0; i < submissions.length; i++) {
         let j;
 
@@ -118,32 +107,34 @@ function getLearnerData(course, ag, submissions) {
         //debug
         //console.log(vLocalID)
     }
+    
+    vTotalNumberOfStudent = vUniqueLearnerIDCount.length;
+    console.log(`Total number of student in Submissions: ${vUniqueLearnerIDCount.length}`);
+//#endregion display total number of students
 
-    console.log(`Total number of student in Submission: ${vUniqueLearnerIDCount.length}`);
-    //#endregion display total number of students
+// display total number of possible Assignments per student
+vTotalNumberOfAssignments = ag.assignments.length;
+console.log(`Total number of possible assignments in submission: ${vTotalNumberOfAssignments}`)
+
+// display current total number of assignments turned out of total possible
+// display avg total assignment turn in of student
+// display total Assignments turned in on-time vs total possible
+// display total Assignments turned in late vs total possible
+// display avg grade of student of submissions.
+// display avg grade per assignment of submissions.
+
+// display student information case by case
+//     display student overall grade avg
+//     display student ranking grade
+//     dispaly number of assignments turned in vs total number of assignments
+//     display assignment information case by case
+//         display assignment ID
+//         display if assignment was submitted
+//             display grade of assignment vs avg grade in submissions
+//             display days need to complete vs avg days needed to complete in submissions
 
 
-    // display total number of possible Assignments per student
-    // display current total number of assignments turned out of total possible
-    // display avg total assignment turn in of student
-    // display total Assignments turned in on-time vs total possible
-    // display total Assignments turned in late vs total possible
-    // display avg grade of student of submissions.
-    // display avg grade per assignment of submissions.
 
-    // display student information case by case
-    //     display student overall grade avg
-    //     display student ranking grade
-    //     dispaly number of assignments turned in vs total number of assignments
-    //     display assignment information case by case
-    //         display assignment ID
-    //         display if assignment was submitted
-    //             display grade of assignment vs avg grade in submissions
-    //             display days need to complete vs avg days needed to complete in submissions
-
-
-
-    //return result;
 }
 
 function isAssignmentOntime() {
