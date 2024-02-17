58aa8ae0be1a3d5ae15bf1f48122635d700e8899
Andrew McClain <86428905+drew020@users.noreply.github.com>
2024-02-16 21:33:26 -0500

    region display total number of students

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+//#region Objects and array
+
 // The provided course information.
-// Not relavent to aignment in anyway
 const CourseInfo = {
     id: 451,
     name: "Introduction to JavaScript"
 };
 
-
 // The provided assignment group.
 const AssignmentGroup = {
     id: 12345,
     name: "Fundamentals of JavaScript",
-    course_id: 451, //Not usedf
+    course_id: 451,
     group_weight: 25,
     assignments: [
         {
@@ -77,35 +77,79 @@ const LearnerSubmissions = [
         }
     }
 ];
+//#endregion Objects and array
 
-function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-        {
-            id: 125,
-            avg: 0.985, // (47 + 150) / (50 + 150)
-            1: 0.94, // 47 / 50
-            2: 1.0 // 150 / 150
-        },
-        {
-            id: 132,
-            avg: 0.82, // (39 + 125) / (50 + 150)
-            1: 0.78, // 39 / 50
-            2: 0.833 // late: (140 - 15) / 150
-        }
-    ];
+//#region global variables
 
-    return result;
-}
-
+//main process 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+//#endregion global variables
 
+//#region main process
 console.log(result);
+//#endregion main process
+
+//#region Functions
+function getLearnerData(course, ag, submissions) {
+    // Cast Argument Array to use methods
+    submission = [];
+
+    //Cast Argument to Obj to use methods
+    ag = {};
+    course = {};
+
+    //Function Variables
+    let vTotalNumberOfStudent = 0;
+    let vUniqueLearnerIDCount = [];
+
+    //#region display total number of students
+    for (let i = 0; i < submissions.length; i++) {
+        let j;
+
+        if (vUniqueLearnerIDCount.includes(submissions[i].learner_id)) {
+            //Debug
+            //console.log("got match");
+        }
+        else {
+            //console.log("not match");
+            vUniqueLearnerIDCount.push(submissions[i].learner_id)
+            j++
+        }
+        //debug
+        //console.log(vLocalID)
+    }
+
+    console.log(`Total number of student in Submission: ${vUniqueLearnerIDCount.length}`);
+    //#endregion display total number of students
 
 
+    // display total number of possible Assignments per student
+    // display current total number of assignments turned out of total possible
+    // display avg total assignment turn in of student
+    // display total Assignments turned in on-time vs total possible
+    // display total Assignments turned in late vs total possible
+    // display avg grade of student of submissions.
+    // display avg grade per assignment of submissions.
+
+    // display student information case by case
+    //     display student overall grade avg
+    //     display student ranking grade
+    //     dispaly number of assignments turned in vs total number of assignments
+    //     display assignment information case by case
+    //         display assignment ID
+    //         display if assignment was submitted
+    //             display grade of assignment vs avg grade in submissions
+    //             display days need to complete vs avg days needed to complete in submissions
+
+
+
+    //return result;
+}
 
 function isAssignmentOntime() {
     let rBool;
     return (rBool);
 }
+//#endregion Functions
+
 
