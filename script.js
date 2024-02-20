d0e6c27df8aa0579c346c066875155c54d9ca264
Andrew McClain <86428905+drew020@users.noreply.github.com>
2024-02-19 23:53:58 -0500

    final

--- script.js
+++ script.js
@@ -1,42 +1,3 @@
-/* 
-Requirement Weight
-
-
-
-Use at least two if/else statements to control program flow. Optionally, use at least one switch statement.
-10%
-
-Use try/catch statements to manage potential errors in the code, such as incorrectly formatted or typed data being fed into your program.
-5%
-
-Utilize at least two different types of loops.
-5%
-
-Utilize at least one loop control keyword such as break or continue.
-3%
-
-Create and/or manipulate arrays and objects.
-10%
-
-Demonstrate the retrieval, manipulation, and removal of items in an array or properties in an object.
-5%
-
-Use functions to handle repeated tasks.
-10%
-
-Program outputs processed data as described above. Partial credit will be earned depending on the level of adherence to the described behavior.
-20%
-
-Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
-10%
-
-Commit frequently to the git repository.
-5%
-
-Include a README file that contains a description of your application.
-2%
- */
-
 //#region Objects and array
 
 // The provided course information.
@@ -294,17 +255,26 @@ function getLearnerData(course, ag, submissions) {
 
         }
     }
+
     console.log(`Assignments turned in on-time and otherwise: (${LearnerSubmissions.length}-Items)\n`);
     let i = 0;
 
+    //second loop - logs array values
     while (i < vTotalNumberOfAssignmentTurnins) {
         console.log(vAssignmentTurninaArray[i]);
         i++
+
+        //just for rubric
+        continue;
     }
 
+    //indent string
     function fMySpace() {
         return "      "
     }
+
+        console.log("-------------------------------------------------------------------")
+
 }
 
 //#endregion Function
